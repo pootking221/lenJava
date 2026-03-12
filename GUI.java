94d62c71b464d61c5c749239c00c171425e298c2
Thitikorn <pootking221@gmail.com>
2026-03-12 22:33:50 +0700

    using class hello

--- GUI.java
+++ GUI.java
@@ -1,6 +1,8 @@
 public class GUI {
     public static void main(String[] args) {
+        Hello hello = new Hello("Username");
         System.out.println("Hello");
-        System.out.println("World");
+        hello.display();
+        
     }
 }
