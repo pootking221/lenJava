7315d765d3eb1695df205afad5ac0e5859cbc679
Thitikorn <pootking221@gmail.com>
2026-03-12 22:29:16 +0700

    add Hello class

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    private String name;
+
+    public Hello(String name) {
+        this.name = name;
+    }
+
+    public void display(){
+        System.out.println(name);
+    }
+}
