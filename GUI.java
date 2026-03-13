cb6f6c2648c709693494eed85315e2582a7ad12c
Thitikorn <pootking221@gmail.com>
2026-03-13 09:08:50 +0700

    add gui

--- GUI.java
+++ GUI.java
@@ -1,8 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class GUI {
     public static void main(String[] args) {
-        Hello hello = new Hello("Username");
-        System.out.println("Hello");
-        hello.display();
-        
+        JFrame window = new JFrame();
+        JTextArea t1, t2;
+        t1 = new JTextArea(10, 30);
+        t2 = new JTextArea(10, 30);
+        Container container = window.getContentPane();
+        container.setLayout(new FlowLayout());
+        container.add(t1);
+        container.add(t2);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(500, 500);
+        window.setVisible(true);
+        window.show();
     }
 }
